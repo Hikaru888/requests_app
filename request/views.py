af43f5e40b8534405f13fbd469228fff1ac1bfdd
Hikaru <hi8ka1ru@icloud.com>
2024-01-23 10:35:25 +0900

    選択項目完了

--- request/views.py
+++ request/views.py
@@ -1,33 +1,39 @@
 import requests
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {'message': "今日の天気の知りたい場所を選択してください。"}
+    return render(request, 'request/index.html', context)
+    
+
 
 def get_weather(request):
-    url = 'https://weather.tsukumijima.net/api/forecast'
+    url = "https://weather.tsukumijima.net/api/forecast"
 
     params = {
-        'city': 'Osaka',
+        "city": "Osaka",
     }
 
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        
-        city = data['location']['city']
-        date = data['forecasts'][0]['date']
-        weather_icon = data['forecasts'][0]['image']['url']
-        temperature = data['forecasts'][0]['temperature']
-        weather_description = data['forecasts'][0]['telop']
+        city = data["location"]["city"]
+        date = data["forecasts"][0]["date"]
+        weather_icon = data["forecasts"][0]["image"]["url"]
+        temperature = data["forecasts"][0]["temperature"]
+        weather_description = data["forecasts"][0]["telop"]
 
-        context = {
-            'city': city,
-            'date': date,
-            'weather_icon': weather_icon,
-            'temperature': temperature,
-            'weather_description': weather_description,
+        contexts = {
+            "city": city,
+            "date": date,
+            "weather_icon": weather_icon,
+            "temperature": temperature,
+            "weather_description": weather_description,
         }
 
-        return render(request, 'weather.html', context)
+        return render(request, "weather.html", context)
     else:
-        
-        return render(request, 'error.html')
+        return render(request, "error.html")
