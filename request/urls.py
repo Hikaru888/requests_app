f3257f49119e5ed63d79fadced9ac25145401335
Hikaru <hi8ka1ru@icloud.com>
2024-01-31 19:17:21 +0900

    完成

--- request/urls.py
+++ request/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('get_weather/', views.get_weather, name='get_weather'),
 ]
--- request/views.py
+++ request/views.py
@@ -4,36 +4,43 @@ from django.http import HttpResponse
 
 
 def index(request):
-    context = {'message': "今日の天気の知りたい場所を選択してください。"}
-    return render(request, 'request/index.html', context)
+    return render(request, 'request/index.html')
     
 
 
 def get_weather(request):
-    url = "https://weather.tsukumijima.net/api/forecast"
-
+    city_code = request.GET.get('検索フォーム')
+    
     params = {
-        "city": "Osaka",
+        "city": city_code,
     }
 
-    response = requests.get(url, params=params)
+    response = requests.get("https://weather.tsukumijima.net/api/forecast", params=params)
+
+    print("Url:", response.url)
+
+    print("Status_code:", response.status_code)
 
     if response.status_code == 200:
         data = response.json()
+        print("Data:", data)
+
         city = data["location"]["city"]
         date = data["forecasts"][0]["date"]
         weather_icon = data["forecasts"][0]["image"]["url"]
-        temperature = data["forecasts"][0]["temperature"]
         weather_description = data["forecasts"][0]["telop"]
+        max_temperature = data["forecasts"][0]["temperature"]["max"]
+        min_temperature = data["forecasts"][0]["temperature"]["min"]
 
         contexts = {
             "city": city,
             "date": date,
             "weather_icon": weather_icon,
-            "temperature": temperature,
             "weather_description": weather_description,
+            "max_temperature": max_temperature,
+            "min_temperature": min_temperature,
         }
 
-        return render(request, "weather.html", context)
+        return render(request, "request/weather.html", contexts)
     else:
-        return render(request, "error.html")
+        return render(request, "request/index.html")
